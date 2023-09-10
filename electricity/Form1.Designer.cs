9e20c776a286d590b139fbad902405fe47455c85
Thatchaphum Jaidee <134765926+Pipe2Bot@users.noreply.github.com>
2023-09-10 16:13:34 +0700

    Add files via upload

---
+++ electricity/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace electricity
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblvoltage = new Label();
+            lbltname = new Label();
+            tbname = new TextBox();
+            label1 = new Label();
+            tbunit = new TextBox();
+            ddlmain = new ComboBox();
+            label2 = new Label();
+            bttcal = new Button();
+            lbelec = new Label();
+            lbshowname = new Label();
+            lblname = new Label();
+            lblcost = new Label();
+            tbvoltage = new TextBox();
+            lbldemand = new Label();
+            tbdemand = new TextBox();
+            bgsub = new Button();
+            SuspendLayout();
+            // 
+            // lblvoltage
+            // 
+            lblvoltage.AutoSize = true;
+            lblvoltage.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            lblvoltage.Location = new Point(14, 220);
+            lblvoltage.Name = "lblvoltage";
+            lblvoltage.Size = new Size(159, 33);
+            lblvoltage.TabIndex = 11;
+            lblvoltage.Text = "voltage :";
+            // 
+            // lbltname
+            // 
+            lbltname.AutoSize = true;
+            lbltname.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            lbltname.ForeColor = SystemColors.ActiveCaptionText;
+            lbltname.Location = new Point(62, 19);
+            lbltname.Name = "lbltname";
+            lbltname.Size = new Size(111, 33);
+            lbltname.TabIndex = 0;
+            lbltname.Text = "name :";
+            // 
+            // tbname
+            // 
+            tbname.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            tbname.Location = new Point(179, 12);
+            tbname.Name = "tbname";
+            tbname.Size = new Size(292, 40);
+            tbname.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(45, 85);
+            label1.Name = "label1";
+            label1.Size = new Size(127, 33);
+            label1.TabIndex = 2;
+            label1.Text = "units :";
+            // 
+            // tbunit
+            // 
+            tbunit.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            tbunit.Location = new Point(179, 82);
+            tbunit.Name = "tbunit";
+            tbunit.Size = new Size(127, 40);
+            tbunit.TabIndex = 3;
+            tbunit.KeyPress += tbunit_KeyPress;
+            // 
+            // ddlmain
+            // 
+            ddlmain.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            ddlmain.FormattingEnabled = true;
+            ddlmain.Items.AddRange(new object[] { "1 บ้านอยู่อาศัย", "3 กิจการขนาดกลาง", "6 องค์กรที่ไม่แสวงหากำไร" });
+            ddlmain.Location = new Point(178, 147);
+            ddlmain.Name = "ddlmain";
+            ddlmain.Size = new Size(292, 41);
+            ddlmain.TabIndex = 4;
+            ddlmain.SelectedIndexChanged += ddlmain_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(62, 150);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 33);
+            label2.TabIndex = 5;
+            label2.Text = "type :";
+            // 
+            // bttcal
+            // 
+            bttcal.BackColor = SystemColors.MenuHighlight;
+            bttcal.Font = new Font("Unispace", 22.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            bttcal.ForeColor = SystemColors.ButtonHighlight;
+            bttcal.Location = new Point(33, 376);
+            bttcal.Name = "bttcal";
+            bttcal.Size = new Size(286, 91);
+            bttcal.TabIndex = 6;
+            bttcal.Text = "calculate";
+            bttcal.UseVisualStyleBackColor = false;
+            bttcal.Click += bttcal_Click;
+            // 
+            // lbelec
+            // 
+            lbelec.AutoSize = true;
+            lbelec.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            lbelec.Location = new Point(367, 429);
+            lbelec.Name = "lbelec";
+            lbelec.Size = new Size(111, 33);
+            lbelec.TabIndex = 7;
+            lbelec.Text = "cost :";
+            // 
+            // lbshowname
+            // 
+            lbshowname.AutoSize = true;
+            lbshowname.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            lbshowname.Location = new Point(367, 376);
+            lbshowname.Name = "lbshowname";
+            lbshowname.Size = new Size(111, 33);
+            lbshowname.TabIndex = 8;
+            lbshowname.Text = "name :";
+            // 
+            // lblname
+            // 
+            lblname.AutoSize = true;
+            lblname.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            lblname.Location = new Point(475, 376);
+            lblname.Name = "lblname";
+            lblname.Size = new Size(111, 33);
+            lblname.TabIndex = 9;
+            lblname.Text = "name :";
+            // 
+            // lblcost
+            // 
+            lblcost.AutoSize = true;
+            lblcost.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            lblcost.Location = new Point(475, 429);
+            lblcost.Name = "lblcost";
+            lblcost.Size = new Size(111, 33);
+            lblcost.TabIndex = 10;
+            lblcost.Text = "name :";
+            // 
+            // tbvoltage
+            // 
+            tbvoltage.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            tbvoltage.Location = new Point(179, 217);
+            tbvoltage.Name = "tbvoltage";
+            tbvoltage.Size = new Size(127, 40);
+            tbvoltage.TabIndex = 12;
+            tbvoltage.KeyPress += tbvoltage_KeyPress;
+            // 
+            // lbldemand
+            // 
+            lbldemand.AutoSize = true;
+            lbldemand.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            lbldemand.Location = new Point(14, 292);
+            lbldemand.Name = "lbldemand";
+            lbldemand.Size = new Size(239, 33);
+            lbldemand.TabIndex = 13;
+            lbldemand.Text = "Power demand :";
+            // 
+            // tbdemand
+            // 
+            tbdemand.BackColor = SystemColors.HighlightText;
+            tbdemand.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
+            tbdemand.Location = new Point(259, 289);
+            tbdemand.Name = "tbdemand";
+            tbdemand.Size = new Size(132, 40);
+            tbdemand.TabIndex = 14;
+            tbdemand.KeyPress += tbdemand_KeyPress;
+            // 
+            // bgsub
+            // 
+            bgsub.BackColor = Color.Chocolate;
+            bgsub.Location = new Point(341, 354);
+            bgsub.Name = "bgsub";
+            bgsub.Size = new Size(647, 135);
+            bgsub.TabIndex = 15;
+            bgsub.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Bisque;
+            ClientSize = new Size(1000, 501);
+            Controls.Add(tbdemand);
+            Controls.Add(lbldemand);
+            Controls.Add(tbvoltage);
+            Controls.Add(lblvoltage);
+            Controls.Add(lblcost);
+            Controls.Add(lblname);
+            Controls.Add(lbshowname);
+            Controls.Add(lbelec);
+            Controls.Add(bttcal);
+            Controls.Add(label2);
+            Controls.Add(ddlmain);
+            Controls.Add(tbunit);
+            Controls.Add(label1);
+            Controls.Add(tbname);
+            Controls.Add(lbltname);
+            Controls.Add(bgsub);
+            ForeColor = SystemColors.ActiveCaptionText;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbltname;
+        private TextBox tbname;
+        private Label label1;
+        private TextBox tbunit;
+        private ComboBox ddlmain;
+        private Label label2;
+        private Button bttcal;
+        private Label lbelec;
+        private Label lbshowname;
+        private Label lblname;
+        private Label lblcost;
+        private TextBox tbvoltage;
+        private Label lblvoltage;
+        private Label lbldemand;
+        private TextBox tbdemand;
+        private Button bgsub;
+    }
+}
---
+++ electricity/Form1.cs
@@ -0,0 +1,328 @@
+﻿namespace electricity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblname.Text = "";
+            lblcost.Text = "";
+            lbshowname.Visible = false;
+            lbelec.Visible = false;
+            lblvoltage.Visible = false;
+            tbvoltage.Visible = false;
+            lbldemand.Visible = false;
+            tbdemand.Visible = false;
+            bgsub.Visible = false;
+
+        }
+
+
+        private void bttcal_Click(object sender, EventArgs e)
+        {
+            if (tbname.Text != "" && tbunit.Text != "" && ddlmain.Text != "")
+            {
+                lbshowname.Visible = false;
+                lbelec.Visible = false;
+                bgsub.Visible = false;
+                double unit, cost, costv;
+
+                unit = 0;
+                unit = Convert.ToDouble(tbunit.Text);
+                if (ddlmain.SelectedIndex == 0)
+                {
+                    if (unit > 150)
+                    {
+                        double x1, x2, plus = 38.22;
+
+                        x1 = 150 * 3.2484;
+                        x2 = 250 * 4.2218;
+                        if (unit >= 400)
+                        {
+                            cost = (((unit - 400) * 4.4217) + x1 + x2) + plus;
+                            costv = cost * 1.07;
+                            lbshowname.Visible = true;
+                            lbelec.Visible = true;
+                            lblcost.Text = Convert.ToString(costv) + " THB";
+                            lblname.Text = tbname.Text;
+                            bgsub.Visible = true;
+                        }
+                        else if (unit >= 151)
+                        {
+                            cost = (((unit - 150) * 4.2218) + x1) + plus;
+                            costv = cost * 1.07;
+                            lbshowname.Visible = true;
+                            lbelec.Visible = true;
+                            lblcost.Text = Convert.ToString(costv) + " THB";
+                            lblname.Text = tbname.Text;
+                            bgsub.Visible = true;
+                        }
+
+                    }
+                    else
+                    {
+                        double x1, x2, x3, x4, plus = 8.19;
+                        x1 = 15 * 2.3488;
+                        x2 = 10 * 2.9882;
+                        x3 = 10 * 3.2405;
+                        x4 = 65 * 3.6237;
+                        if (unit >= 101)
+                        {
+                            cost = (((unit - 100) * 3.7171) + x1 + x2 + x3 + x4 + plus);
+                            costv = cost * 1.07;
+                            lbshowname.Visible = true;
+                            lbelec.Visible = true;
+                            lblcost.Text = Convert.ToString(costv) + " THB";
+                            lblname.Text = tbname.Text;
+                            bgsub.Visible = true;
+                        }
+                        else if (unit >= 36)
+                        {
+                            cost = (((unit - 35) * 3.6237) + x1 + x2 + x3 + plus);
+                            costv = cost * 1.07;
+                            lbshowname.Visible = true;
+                            lbelec.Visible = true;
+                            lblcost.Text = Convert.ToString(costv) + " THB";
+                            lblname.Text = tbname.Text;
+                            bgsub.Visible = true;
+                        }
+                        else if (unit >= 26)
+                        {
+                            cost = (((unit - 25) * 3.2405) + x1 + x2 + plus);
+                            costv = cost * 1.07;
+                            lbshowname.Visible = true;
+                            lbelec.Visible = true;
+                            lblcost.Text = Convert.ToString(costv) + " THB";
+                            lblname.Text = tbname.Text;
+                            bgsub.Visible = true;
+                        }
+                        else if (unit >= 16)
+                        {
+                            cost = (((unit - 15) * 2.9882) + x1 + plus);
+                            costv = cost * 1.07;
+                            lbshowname.Visible = true;
+                            lbelec.Visible = true;
+                            lblcost.Text = Convert.ToString(costv) + " THB";
+                            lblname.Text = tbname.Text;
+                            bgsub.Visible = true;
+                        }
+                        else
+                        {
+                            cost = ((unit * 2.3488) + plus);
+                            costv = cost * 1.07;
+                            lbshowname.Visible = true;
+                            lbelec.Visible = true;
+                            lblcost.Text = Convert.ToString(costv) + " THB";
+                            lblname.Text = tbname.Text;
+                            bgsub.Visible = true;
+                        }
+                    }
+                }
+                else if (ddlmain.SelectedIndex == 1)
+                {
+                    double volt, plus = 312.24, x1 = 3.1751, x2 = 3.1471, x3 = 3.1097, demand;
+                    double y1 = 221.50, y2 = 196.26, y3 = 175.70;
+
+
+                    if (tbname.Text != "" && tbunit.Text != "" && ddlmain.Text != "" && tbdemand.Text != "" && tbvoltage.Text != "")
+                    {
+                        volt = Convert.ToDouble(tbvoltage.Text);
+                        demand = Convert.ToDouble(tbdemand.Text);
+
+
+                        if (volt >= 69)
+                        {
+                            cost = (demand * y3) + (unit * x3) + plus;
+                            costv = cost * 1.07;
+                            lbshowname.Visible = true;
+                            lbelec.Visible = true;
+                            lblcost.Text = Convert.ToString(costv) + " THB";
+                            lblname.Text = tbname.Text;
+                            bgsub.Visible = true;
+                        }
+                        else if (volt >= 22 && volt <= 33)
+                        {
+                            cost = (demand * y2) + (unit * x2) + plus;
+                            costv = cost * 1.07;
+                            lbshowname.Visible = true;
+                            lbelec.Visible = true;
+                            lblcost.Text = Convert.ToString(costv) + " THB";
+                            lblname.Text = tbname.Text;
+                            bgsub.Visible = true;
+                        }
+                        else if (volt < 22)
+                        {
+                            cost = (demand * y1) + (unit * x1) + plus;
+                            costv = cost * 1.07;
+                            lbshowname.Visible = true;
+                            lbelec.Visible = true;
+                            lblcost.Text = Convert.ToString(costv) + " THB";
+                            lblname.Text = tbname.Text;
+                            bgsub.Visible = true;
+                        }
+                        else
+                        {
+                            lblname.Text = "";
+                            lblcost.Text = "";
+                            lbshowname.Visible = false;
+                            lbelec.Visible = false;
+                            MessageBox.Show("Invalid voltage input");
+                            bgsub.Visible = false;
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incomplete input" + "\n" + "plese check your input" + "\n" + "and try again");
+                    }
+
+                }
+                else if (ddlmain.SelectedIndex == 2)
+                {
+                    double plus = 312.24, plus22 = 20.00, volt;
+                    if (tbname.Text != "" && tbunit.Text != "" && ddlmain.Text != "" && tbvoltage.Text != "")
+                    {
+                        volt = Convert.ToDouble(tbvoltage.Text);
+                        if (volt >= 69)
+                        {
+                            cost = ((unit * 3.4149) + plus);
+                            costv = cost * 1.07;
+                            lbshowname.Visible = true;
+                            lbelec.Visible = true;
+                            lblcost.Text = Convert.ToString(costv) + " THB";
+                            lblname.Text = tbname.Text;
+                            bgsub.Visible = true;
+                        }
+                        else if (volt >= 22 && volt <= 33)
+                        {
+                            cost = ((unit * 3.5849) + plus);
+                            costv = cost * 1.07;
+                            lbshowname.Visible = true;
+                            lbelec.Visible = true;
+                            lblcost.Text = Convert.ToString(costv) + " THB";
+                            lblname.Text = tbname.Text;
+                            bgsub.Visible = true;
+                        }
+                        else if (volt < 22)
+                        {
+                            if (unit > 10)
+                            {
+                                double x1 = 10 * 2.8013;
+                                cost = (((unit - 10) * 3.8919) + x1 + plus22);
+                                costv = cost * 1.07;
+                                lbshowname.Visible = true;
+                                lbelec.Visible = true;
+                                lblcost.Text = Convert.ToString(costv) + " THB";
+                                lblname.Text = tbname.Text;
+                                bgsub.Visible = true;
+                            }
+                            else
+                            {
+                                cost = (unit * 2.8013) + plus22;
+                                costv = cost * 1.07;
+                                lbshowname.Visible = true;
+                                lbelec.Visible = true;
+                                lblcost.Text = Convert.ToString(costv) + " THB";
+                                lblname.Text = tbname.Text;
+                                bgsub.Visible = true;
+                            }
+                        }
+                        else
+                        {
+                            lblname.Text = "";
+                            lblcost.Text = "";
+                            lbshowname.Visible = false;
+                            lbelec.Visible = false;
+                            MessageBox.Show("Invalid voltage input");
+                            bgsub.Visible = false;
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incomplete input" + "\n" + "plese check your input" + "\n" + "and try again");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Incomplete input" + "\n" + "plese check your input" + "\n" + "and try again");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ddlmain_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlmain.SelectedIndex == 0)
+            {
+                lblvoltage.Visible = false;
+                tbvoltage.Visible = false;
+                lbldemand.Visible = false;
+                tbdemand.Visible = false;
+
+            }
+            else if (ddlmain.SelectedIndex == 1)
+            {
+                lblvoltage.Visible = true;
+                tbvoltage.Visible = true;
+                lbldemand.Visible = true;
+                tbdemand.Visible = true;
+
+            }
+            else
+            {
+                lblvoltage.Visible = true;
+                tbvoltage.Visible = true;
+                lbldemand.Visible = false;
+                tbdemand.Visible = false;
+
+            }
+        }
+
+        private void tbunit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int cInt = Convert.ToInt32(e.KeyChar);
+            if ((int)e.KeyChar >= 48 && (int)e.KeyChar <= 57 || (int)e.KeyChar == 8 || (int)e.KeyChar == 46)
+            {
+                e.Handled = false;
+
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tbvoltage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int cInt = Convert.ToInt32(e.KeyChar);
+            if ((int)e.KeyChar >= 48 && (int)e.KeyChar <= 57 || (int)e.KeyChar == 8 || (int)e.KeyChar == 46)
+            {
+                e.Handled = false;
+
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tbdemand_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int cInt = Convert.ToInt32(e.KeyChar);
+            if ((int)e.KeyChar >= 48 && (int)e.KeyChar <= 57 || (int)e.KeyChar == 8 || (int)e.KeyChar == 46)
+            {
+                e.Handled = false;
+
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
